0712c54da51330be1cb53ca6ac28ee39980a82bd
Nash <sawmebabe@gmail.com>
2016-02-05 01:25:00 +0200

    Added LinkedIn support. Cleaned up url and location hash processing

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,17 +3,31 @@ class User < ActiveRecord::Base
 		def from_onmiauth(auth_hash)
 			user = find_or_create_by(uid: auth_hash['uid'], provider: auth_hash['provider'])
 			user.name = auth_hash['info']['name']
-			user.location = auth_hash['info']['location']
+			user.location = get_social_location(oauth_hash['provider'], auth_hash['info']['location'])
 			user.image_url = auth_hash['info']['image']
-			if auth_hash['provider'] == "Twitter"
-				user.url = auth_hash['info']['urls']['Twitter']
-			elsif auth_hash['provider'] == "Facebook"
-				user.url = auth_hash['info']['urls'][user.provider.capitalize]
-			elsif auth_hash['provider'] == "Google"
-				user.url = auth_hash['info']['urls'][user.provider.capitalize]
-			end
+			user.url = get_social_url_for(oauth_hash["provider"], oauth_hash['info']['urls'])
 			user.save!
 			user
+		end
+
+		private
+
+		def get_social_location(provider, location_hash)
+			case provider
+			when "linkedin"
+				location_hash['name']
+			else
+				location_hash[provider.capitalize]
+			end
+		end
+
+		def get_social_url_for(provider, url_hash)
+			case 
+			when "linkedin"
+				url_hash['public_profile']
+			else
+				url_hash[provider.capitalize]
+			end
 		end
 	end
 end
