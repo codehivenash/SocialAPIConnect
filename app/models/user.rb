d008805b22c23f9d2b10138e684d9a29b43e3bea
Nash <sawmebabe@gmail.com>
2016-02-04 16:05:59 +0200

    First Commit. Added Facebook and Twitter Providers including simple login

---
+++ app/models/user.rb
@@ -0,0 +1,17 @@
+class User < ActiveRecord::Base
+	class << self
+		def from_onmiauth(auth_hash)
+			user = find_or_create_by(uid: auth_hash['uid'], provider: auth_hash['provider'])
+			user.name = auth_hash['info']['name']
+			user.location = auth_hash['info']['location']
+			user.image_url = auth_hash['info']['image']
+			if auth_hash['provider'] == "Twitter"
+				user.url = auth_hash['info']['urls']['Twitter']
+			elsif auth_hash['provider'] == "Facebook"
+				user.url = auth_hash['info']['urls'][user.provider.capitalize]
+			end
+			user.save!
+			user
+		end
+	end
+end
